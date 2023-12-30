07b9a7b9edb61edee6b72e56bd16fc2789106fe1
TheVave <tobyalawson@outlook.com>
2023-12-30 10:51:43 -0800

    Removed old files

--- displayUpdate.bat
+++
@@ -1,4 +0,0 @@
-cd O:\Users\tobya\source\repos\SharpPhysics\SharpPhysicsTester\bin\Debug\net7.0
-del display.exe
-xcopy O:\Users\tobya\source\repos\SharpPhysics\x64\Debug\FastPixelWriter.exe O:\Users\tobya\source\repos\SharpPhysics\SharpPhysicsTester\bin\Debug\net7.0
-ren FastPixelWriter.exe display.exe
