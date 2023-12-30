0bfa7c8c05134eb879fe9317cbb5da8eb44e2358
TheVave <tobyalawson@outlook.com>
2023-12-30 15:46:43 -0800

    Changed the shader organization

---
+++ SharpPhysics/Renderer/Shaders/Shader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OpenGL.GL;
+using GLFW;
+using SharpPhysics.Utilities.MISC.Errors;
+
+namespace SharpPhysics.Renderer.Shaders
+{
+	public class Shader
+	{
+		public string VertexCode;
+		public string FragmentCode;
+
+		public uint ProgramID;
+
+		public Shader(string vertexCode, string fragmentCode)
+		{
+			VertexCode = vertexCode;
+			FragmentCode = fragmentCode;
+			this.Load();
+		}
+
+
+		public virtual void Load() 
+		{
+			uint vs = glCreateShader(GL_VERTEX_SHADER);
+			glShaderSource(vs, VertexCode);
+			glCompileShader(vs);
+
+			uint fs = glCreateShader(GL_FRAGMENT_SHADER);
+			glShaderSource(fs, FragmentCode);
+			glCompileShader(fs);
+
+			int[] vsstatus = glGetShaderiv(vs, GL_COMPILE_STATUS, 1);
+			int[] fsstatus = glGetShaderiv(fs, GL_COMPILE_STATUS, 1);
+
+			if (vsstatus[0] == 0 || fsstatus[0] == 0)
+			{
+				ErrorHandler.ThrowError("External/internal error, Shader compilation failed.\n" +
+					" You may have supplied incorrect data for the fragment/vertex shader code. " +
+					"\nThe shaders are programmed in HLSL.", false);
+				ErrorHandler.ThrowError("Complete error (fs): " + glGetShaderInfoLog(fs), false);
+				ErrorHandler.ThrowError("Complete error (vs): " + glGetShaderInfoLog(vs), true);
+			}
+
+			ProgramID = glCreateProgram();
+			glAttachShader(ProgramID, vs);
+			glAttachShader(ProgramID, fs);
+
+			glLinkProgram(ProgramID);
+
+			// detaching unneeded opengl mem
+			glDetachShader(ProgramID, vs);
+			glDetachShader(ProgramID, fs);
+			glDeleteShader(vs);
+			glDeleteShader(fs);
+		}
+
+		public void Use()
+		{
+			glUseProgram(ProgramID);
+		}
+	}
+}
--- SharpPhysics/Renderer/Tests/StandardTest.cs
+++ SharpPhysics/Renderer/Tests/StandardTest.cs
@@ -11,7 +11,10 @@ namespace SharpPhysics.Renderer.Tests
 {
 	internal class StandardTest : Game
 	{
-		
+		uint vao;
+		uint vbo;
+
+		Shaders.Shader shader;
 
 		public StandardTest(int initialWindowWidth, int initialWindowHeight, string windowTitle) : base(initialWindowWidth, initialWindowHeight, windowTitle)
 		{
@@ -19,8 +22,13 @@ namespace SharpPhysics.Renderer.Tests
 
 		protected override void Draw()
 		{
-			glClearColor((float)(MathF.Sin((float)GameTime.TotalElapsedSeconds) * 0.5) + 0.5f, 0, 0, 1);
+			glClearColor(0, 0, 0, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			shader.Use();
+
+			glBindVertexArray(vao);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
 
 			Glfw.SwapBuffers(Display.DisplayManager.Window);
 		}
@@ -29,8 +37,61 @@ namespace SharpPhysics.Renderer.Tests
 		{
 		}
 
-		protected override void LoadContent()
+		protected unsafe override void LoadContent()
 		{
+			// shaders
+			string vertexShader = @"#version 330 core
+                                    layout (location = 0) in vec2 aPosition;
+                                    layout (location = 1) in vec3 aColor;
+                                    out vec4 vertexColor;
+    
+                                    void main() 
+                                    {
+                                        vertexColor = vec4(aColor.rgb, 1.0);
+                                        gl_Position = vec4(aPosition.xy, 0, 1.0);
+                                    }";
+
+			string fragmentShader = @"#version 330 core
+                                    out vec4 FragColor;
+                                    in vec4 vertexColor;
+
+                                    void main() 
+                                    {
+                                        FragColor = vertexColor;
+                                    }";
+			shader = new Shaders.Shader(vertexShader, fragmentShader);
+
+			// creating vao and vbo
+			vao = glGenVertexArray();
+			vbo = glGenBuffer();
+
+			// binding vao and vbo
+			glBindVertexArray(vao);
+			glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+			float[] vertices =
+			{
+				-0.5f, 0.5f, 1f, 0f, 0f, // top left
+                0.5f, 0.5f, 0f, 1f, 0f,// top right
+                -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+
+                0.5f, 0.5f, 0f, 1f, 0f,// top right
+                0.5f, -0.5f, 0f, 1f, 1f, // bottom right
+                -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+            };
+
+			fixed (float* floatPtr = &vertices[0])
+				glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, floatPtr, GL_STATIC_DRAW);
+			// vertexes
+			glVertexAttribPointer(0, 2, GL_FLOAT, false, 5 * sizeof(float), (void*)0);
+			glEnableVertexAttribArray(0);
+
+			// colors
+			glVertexAttribPointer(1, 3, GL_FLOAT, false, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+			glEnableVertexAttribArray(1);
+
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glBindVertexArray(0);
 		}
 
 		protected override void Update()
