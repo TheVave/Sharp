38e6d95660b024d842a1a6930047fdd6b500edf1
TheVave <tobyalawson@outlook.com>
2024-01-03 16:44:16 -0800

    Finished the TexturePreloader.GetFilePreloadedInfo method.

--- SharpPhysics/Renderer/Textures/TexturePreloader.cs
+++ SharpPhysics/Renderer/Textures/TexturePreloader.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SharpPhysics.Utilities.MathUtils;
+using SharpPhysics.Utilities.MISC;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace SharpPhysics.Renderer.Textures
 {
-	internal class TexturePreloader
+	public static class TexturePreloader
 	{
+		public static Texture GetFilePreloadedInfo(string name)
+		{
+			string[] txtrContents = File.ReadAllLines($@"{Environment.CurrentDirectory}\Ctnt\Txtrs\{name}");
+			Texture texture = new();
+			texture.ImageWidth = (short)GenericMathUtils.ParseStrToInt32(txtrContents[0]);
+			texture.ImageHeight = (short)GenericMathUtils.ParseStrToInt32(txtrContents[0].Substring(txtrContents[0].IndexOf(' ') + 1));
+			List<byte> imageContents = new List<byte>();
+			foreach (string line in txtrContents) imageContents.AddRange(Encoding.GetEncoding("UTF-8").GetBytes(line));
+			texture.ImageBytes = new byte[imageContents.Count];
+			Array.Copy(imageContents.ToArray(), texture.ImageBytes, imageContents.Count);
+			return texture;
+		}
 	}
 }
