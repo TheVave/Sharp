1b6b5b1a678e1ba7d6d089c3c718feef8a0fb6df
TheVave <tobyalawson@outlook.com>
2024-01-04 12:28:00 -0800

    Added TexturePreloader class

--- SharpPhysics/2d/ObjectRepresentation/Hierarchys/_2dSceneHierarchy.cs
+++ SharpPhysics/2d/ObjectRepresentation/Hierarchys/_2dSceneHierarchy.cs
@@ -5,8 +5,8 @@ namespace SharpPhysics._2d.ObjectRepresentation.Hierarchies
 {
 	public class _2dSceneHierarchy
 	{
-		public _2dSimulatedObject[] Objects = new _2dSimulatedObject[0];
-		public byte HierarchyId = 0;
+        public _2dSimulatedObject[] Objects = [];
+        public byte HierarchyId = 0;
 		public static void RegisterObject(byte hierarchyId)
 		{
 			ErrorHandler.ThrowNotImplementedExcepetion();
--- SharpPhysics/Renderer/Textures/TexturePreloader.cs
+++ SharpPhysics/Renderer/Textures/TexturePreloader.cs
@@ -1,4 +1,9 @@
-﻿using SharpPhysics.Utilities.MathUtils;
+﻿using GLFW;
+using SharpPhysics.Utilities.MathUtils;
+using SharpPhysics.Utilities.MISC.Errors;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace SharpPhysics.Renderer.Textures
@@ -7,15 +12,93 @@ namespace SharpPhysics.Renderer.Textures
 	{
 		public static Texture GetFilePreloadedInfo(string name)
 		{
-			string[] txtrContents = File.ReadAllLines($@"{Environment.CurrentDirectory}\Ctnt\Txtrs\{name}");
+			string[] txtrContents = Array.Empty<string>();
+			try
+			{
+				txtrContents = File.ReadAllLines($@"{Environment.CurrentDirectory}\Ctnt\Txtrs\{name}");
+			}
+			catch
+			{
+				ErrorHandler.ThrowError("Error, External Error, Missing texture", true);
+			}
 			Texture texture = new();
-			texture.ImageWidth = (short)GenericMathUtils.ParseStrToInt32(txtrContents[0]);
-			texture.ImageHeight = (short)GenericMathUtils.ParseStrToInt32(txtrContents[0].Substring(txtrContents[0].IndexOf(' ') + 1));
-			List<byte> imageContents = new List<byte>();
-			foreach (string line in txtrContents) imageContents.AddRange(Encoding.GetEncoding("UTF-8").GetBytes(line));
-			texture.ImageBytes = new byte[imageContents.Count];
-			Array.Copy(imageContents.ToArray(), texture.ImageBytes, imageContents.Count);
+			try
+			{
+				texture.ImageWidth = (short)GenericMathUtils.ParseStrToInt32(txtrContents[0]);
+				texture.ImageHeight = (short)GenericMathUtils.ParseStrToInt32(txtrContents[0][(txtrContents[0].IndexOf(' ') + 1)..]);
+			}
+			catch (ArgumentOutOfRangeException e)
+			{
+				ErrorHandler.ThrowError("Error, External Error, Invalid texture format.", true);
+			}
+			catch (InvalidCastException e)
+			{
+				ErrorHandler.ThrowError("Error, External error, Image too big for short. Image width/height can't exceed 32000.", true);
+			}
+			List<byte> imageContents = new();
+			try
+			{
+				foreach (string line in txtrContents) imageContents.AddRange(Encoding.GetEncoding("UTF-8").GetBytes(line));
+			}
+			catch (ArgumentNullException e)
+			{
+				ErrorHandler.ThrowError("Error, External Error, Texture does not have enough info.", true);
+			}
+			catch (ArgumentException e)
+			{
+				ErrorHandler.ThrowError("Error, Unknown error, Exact error: " + e, true);
+			}
+			try
+			{
+				texture.ImageBytes = new byte[imageContents.Count];
+			}
+			catch
+			{
+				ErrorHandler.ThrowError("Error, External Error, Image uses too much memory.", true);
+			}
+			try
+			{
+				Array.Copy(imageContents.ToArray(), texture.ImageBytes, imageContents.Count);
+			}
+			catch (System.Exception e)
+			{
+				ErrorHandler.ThrowError("Error, Internal Error, Possible missing data.", true);
+			}
 			return texture;
+		}
+		public static void SaveTexture(string name, Bitmap bmp)
+		{
+			MemoryStream strm = new MemoryStream();
+			try
+			{
+				bmp.Save(strm, ImageFormat.Bmp);
+			}
+			catch (ExternalException ex) 
+			{
+				Console.Error.WriteLine("Non-Windows OS, SharpPhysics is only compatible with Windows.");
+			}
+			catch (ArgumentNullException e)
+			{
+				ErrorHandler.ThrowError("Unknown Error, TexturePreloader class. SaveTexture(string, bitmap)", true);
+			}
+			strm.Position = 0;
+			try
+			{
+				byte[] bytes = new byte[strm.Length];
+				strm.Read(bytes, 0, (int)strm.Length);
+			}
+			catch (ObjectDisposedException e)
+			{
+				ErrorHandler.ThrowError("Error, Internal Error, image disposed, " + name, false);
+			}
+			catch (InvalidCastException ice)
+			{
+				ErrorHandler.ThrowError("Error, External Error, Image too big. image size exceeds 4,294,967,296 (2^32), please make the image smaller.", true);
+			}
+			catch (System.Exception e)
+			{
+				ErrorHandler.ThrowError("Error, Internal Error, Unknown exception, TexturePreloader class. Exact error: " + e.Message, true);
+			}
 		}
 	}
 }
